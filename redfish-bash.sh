db1e911d48a1e319289e0ea37f3316a3e8fd5328
Borball Zhai <borball.zh@gmail.com>
2023-08-30 10:53:06 -0400

    add more

--- redfish-bash.sh
+++ redfish-bash.sh
@@ -36,14 +36,33 @@ system(){
   echo "$bmc""$system"
 }
 
+systems(){
+ local system=$(curl -sku "${username_password}" "$bmc"/redfish/v1/Systems | jq -r '.Members[0]."@odata.id"' )
+ curl -sku "${username_password}" "$bmc""$system" |jq
+}
+
 manager(){
   local manager=$(curl -sku "${username_password}" "$bmc"/redfish/v1/Managers | jq -r '.Members[0]."@odata.id"' )
   echo "$bmc""$manager"
 }
 
+managers(){
+  local manager=$(curl -sku "${username_password}" "$bmc"/redfish/v1/Managers | jq -r '.Members[0]."@odata.id"' )
+  curl -sku "${username_password}" "$bmc""$manager" |jq
+}
+
 bios(){
   local system=$(system)
   curl -sku "${username_password}" "$system" |jq -r
+}
+
+nics(){
+  local system=$(system)
+  local ethernet_address=$(curl -sku "${username_password}" "$system" |jq -r '.EthernetInterfaces."@odata.id"')
+  local ethernetInterfaces=$(curl -sku "${username_password}" "$bmc""$ethernet_address" |jq -r '.Members[]."@odata.id"')
+  for ethernetInterface in $ethernetInterfaces; do
+    curl -sku "${username_password}" "$bmc""$ethernetInterface" |jq '{Id,MACAddress,LinkStatus,Status}'
+  done
 }
 
 if [ -n "$cmd" ]; then
