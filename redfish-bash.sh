11311c3c2f78f0b9cba346ba0731ec71101efb54
Borball Zhai <borball.zh@gmail.com>
2024-10-21 12:28:44 -0400

    use netrc-file to protect the username/password

--- redfish-bash.sh
+++ redfish-bash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 #
+umask 0066
+NETRC="${HOME}/.bmc.netrc"
+CURL="curl -k --netrc-file ${NETRC}"
 
 if ! type "yq" > /dev/null; then
   echo "Cannot find yq in the path, please install yq on the node first. ref: https://github.com/mikefarah/yq#install"
@@ -20,8 +23,7 @@ usage(){
   echo "Available commands: "
   echo "  -------------------------------------------"
   echo "  #login BMC"
-  echo "  login [bmc] [username:password]"
-  echo "  login [bmc] [username:password] [kvm_uuid]"
+  echo "  login <bmc> [kvm_uuid]"
   echo "  -------------------------------------------"
   echo "  #bmc server context"
   echo "  #all servers:"
@@ -57,8 +59,7 @@ usage(){
   echo "  storage <ID>"
   echo
   echo "Examples: "
-  echo "  $0 login https://192.168.13.146 Administrator:superuser"
-  echo "  $0 login https://192.168.13.146 Administrator:superuser [kvm_uuid]"
+  echo "  $0 login https://192.168.13.146"
   echo "  $0 servers"
   echo "  $0 server"
   echo "  $0 server 0"
@@ -84,6 +85,15 @@ usage(){
   echo "  $0 get"
   echo "  $0 get /redfish/v1/TelemetryService"
   echo "Run : $0 login before using other commands for the first time."
+  echo ""
+  echo "BMC credential must be saved in $NETRC in following format"
+  echo "machine <BMC_HOST> login <USER_NAME> password <PASSWORD>"
+  echo "default login <USER_NAME> password <PASSWORD>"
+
+}
+
+help(){
+  usage
 }
 
 help(){
@@ -100,15 +110,12 @@ _save_cfg(){
   local total=$(yq ".|length" "$ALL_SERVERS_CFG")
   echo "- index: $total" >> "$ALL_SERVERS_CFG"
   echo "  bmc: $1" >> "$ALL_SERVERS_CFG"
-  echo "  userPass: $2" >> "$ALL_SERVERS_CFG"
 
   echo "index: $total" > "$CURRENT_SERVERS_CFG"
   echo "bmc: $1" >> "$CURRENT_SERVERS_CFG"
-  echo "userPass: $2" >> "$CURRENT_SERVERS_CFG"
 
   yq "$CURRENT_SERVERS_CFG"
   export bmc=$(yq ".bmc" "$CURRENT_SERVERS_CFG")
-  export username_password=$(yq ".userPass" "$CURRENT_SERVERS_CFG")
 }
 
 _pick(){
@@ -118,20 +125,19 @@ _pick(){
 login(){
   local bmc_info=($parameters)
 
-  if [ "${#bmc_info[@]}" -gt 1 ]; then
+  if [ "${#bmc_info[@]}" -gt 0 ]; then
     local bmc="${bmc_info[0]}"
-    local username_password="${bmc_info[1]}"
 
-    local redfish_url="$bmc"/redfish/v1
-    if [ "${#bmc_info[@]}" = 3 ]; then
+    local redfish_url="$bmc"/redfish/v1/Systems
+    if [ "${#bmc_info[@]}" = 2 ]; then
       uuid="${bmc_info[2]}"
       redfish_url="$redfish_url/Systems/$uuid"
     fi
 
-    if [ $(curl -ku "$username_password" -s -o /dev/null -w ''%{http_code}'' "$redfish_url") -eq 200 ]; then
+    if [ $($CURL -s -o /dev/null -w ''%{http_code}'' "$redfish_url") -eq 200 ]; then
       #login succeed
       echo "login successful, will use this server for the following commands."
-      _save_cfg "$bmc" "$username_password"
+      _save_cfg "$bmc"
     else
       echo "login failed, please check."
       exit 1
@@ -176,13 +182,12 @@ server(){
   fi
 
   export bmc=$(yq ".bmc" "$CURRENT_SERVERS_CFG")
-  export username_password=$(yq ".userPass" "$CURRENT_SERVERS_CFG")
   echo
 }
 
 _system(){
   if [ -z "$uuid" ]; then
-    local system=$(curl -sku "${username_password}" "$bmc"/redfish/v1/Systems | jq -r '.Members[0]."@odata.id"' )
+    local system=$($CURL -s "$bmc"/redfish/v1/Systems | jq -r '.Members[0]."@odata.id"' )
   else
     local system=/redfish/v1/Systems/"$uuid"
   fi
@@ -192,7 +197,7 @@ _system(){
 
 _manager(){
   if [ -z "$uuid" ]; then
-    local manager=$(curl -sku "${username_password}" "$bmc"/redfish/v1/Managers | jq -r '.Members[0]."@odata.id"' )
+    local manager=$($CURL -s "$bmc"/redfish/v1/Managers | jq -r '.Members[0]."@odata.id"' )
   else
     local manager=/redfish/v1/Managers/"$uuid"
   fi
@@ -204,11 +209,11 @@ system(){
   local system=$(_system)
   if [ -n "$parameters" ]; then
     attr=$(_pick $parameters)
-    curl -sku "${username_password}" "$system" |jq -r "pick(.$attr)"
+    $CURL -s "$system" |jq -r "pick(.$attr)"
   else
-    curl -sku "${username_password}" "$system" |jq
+    $CURL -s "$system" |jq
   fi
- 
+
 }
 
 manager(){
@@ -216,17 +221,17 @@ manager(){
 
   if [ -n "$parameters" ]; then
     attr=$(_pick $parameters)
-    curl -sku "${username_password}" "$manager" |jq -r "pick(.$attr)"
+    $CURL -s "$manager" |jq -r "pick(.$attr)"
   else
-    curl -sku "${username_password}" "$manager" |jq
+    $CURL -s "$manager" |jq
   fi
 }
 
 bmc-reboot(){
   local manager=$(_manager)
-  local reset=$(curl -sku "${username_password}" "$manager" |jq -r ".Actions.\"#Manager.Reset\".target")
+  local reset=$($CURL -s "$manager" |jq -r ".Actions.\"#Manager.Reset\".target")
   local reset_type="ForceRestart"
-  curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku "${username_password}" \
+  $CURL --globoff  -L -w "%{http_code} %{url_effective}\\n" \
   -H "Content-Type: application/json" -H "Accept: application/json" \
   -d "{\"ResetType\": \"${reset_type}\"}" \
   -X POST "$bmc""$reset"
@@ -234,36 +239,36 @@ bmc-reboot(){
 
 bios(){
   local system=$(_system)
-  local bios=$(curl -sku "${username_password}" "$system"|jq -r '.Bios."@odata.id"')
+  local bios=$($CURL -s "$system"|jq -r '.Bios."@odata.id"')
 
   if [ -n "$parameters" ]; then
     if [[ $parameters =~ "=" ]]; then
       #update bios settings
-      local settings=$(curl -sku "${username_password}" "$bmc""$bios" |jq -r ".\"@Redfish.Settings\".SettingsObject.\"@odata.id\"")
+      local settings=$($CURL -s "$bmc""$bios" |jq -r ".\"@Redfish.Settings\".SettingsObject.\"@odata.id\"")
       local k_v=($(echo $parameters | cut -d "="  --output-delimiter=" " -f 1-))
       local key=${k_v[0]}
       local value=${k_v[1]}
 
-      curl --globoff  -L -w "%{http_code} %{url_effective}\\n"  -ku ${username_password}  \
+      $CURL --globoff  -L -w "%{http_code} %{url_effective}\\n" \
       -H "Content-Type: application/json" -H "Accept: application/json" \
       -d "{\"Attributes\":{\"$key\": \"$value\"}}" \
       -X PATCH "$bmc""$settings"
     else
       #fetch bios settings
       attr=$(_pick $parameters)
-      curl -sku "${username_password}" "$bmc""$bios" |jq -r "pick(.$attr)"
+      $CURL -s "$bmc""$bios" |jq -r "pick(.$attr)"
     fi
   else
-    curl -sku "${username_password}" "$bmc""$bios" |jq
+    $CURL -s "$bmc""$bios" |jq
   fi
 }
 
 eths(){
   local system=$(_system)
-  local ethernet_address=$(curl -sku "${username_password}" "$system" |jq -r '.EthernetInterfaces."@odata.id"')
-  local ethernetInterfaces=$(curl -sku "${username_password}" "$bmc""$ethernet_address" |jq -r '.Members[]."@odata.id"')
+  local ethernet_address=$($CURL -s "$system" |jq -r '.EthernetInterfaces."@odata.id"')
+  local ethernetInterfaces=$($CURL -s "$bmc""$ethernet_address" |jq -r '.Members[]."@odata.id"')
   for ethernetInterface in $ethernetInterfaces; do
-    curl -sku "${username_password}" "$bmc""$ethernetInterface" |jq
+    $CURL -s "$bmc""$ethernetInterface" |jq
   done
 }
 
@@ -271,7 +276,7 @@ power() {
   local system=$(_system)
 
   if [ -z "$parameters" ]; then
-    curl -sku "${username_password}" "$system" |jq -r ".PowerState"
+    $CURL -s "$system" |jq -r ".PowerState"
   else
     local reset_type
     if [ "off" = "$parameters" ]; then
@@ -288,7 +293,7 @@ power() {
     fi
 
     if [ -n "$reset_type" ]; then
-      curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku "${username_password}" \
+      $CURL --globoff  -L -w "%{http_code} %{url_effective}\\n" \
         -H "Content-Type: application/json" -H "Accept: application/json" \
         -d "{\"ResetType\": \"${reset_type}\"}" \
         -X POST "$system"/Actions/ComputerSystem.Reset
@@ -301,18 +306,17 @@ power() {
 virtual-media(){
   local virtual_media_selected
   local manager=$(_manager)
-  local virtual_medias=$(curl -sku "${username_password}" "$manager"/"VirtualMedia" | jq -r '.Members[]."@odata.id"' )
+  local virtual_medias=$($CURL -s "$manager"/"VirtualMedia" | jq -r '.Members[]."@odata.id"' )
   for virtual_media in $virtual_medias; do
-    if [ $(curl -sku "${username_password}" "$bmc""$virtual_media" | jq '.MediaTypes[]' |grep -ciE 'CD|DVD') -gt 0 ]; then
+    if [ $($CURL -s "$bmc""$virtual_media" | jq '.MediaTypes[]' |grep -ciE 'CD|DVD') -gt 0 ]; then
       virtual_media_selected=$virtual_media
       break
     fi
   done
 
   if [ -z "$parameters" ]; then
-    curl -s --globoff -H "Content-Type: application/json" -H "Accept: application/json" \
-      -k -X GET --user "${username_password}" \
-      "$bmc""$virtual_media_selected"| jq
+    $CURL -s --globoff -H "Content-Type: application/json" -H "Accept: application/json" \
+      -X GET "$bmc""$virtual_media_selected"| jq
   else
     local virtual_media_ops=($parameters)
     local virtual_media_action="${virtual_media_ops[0]}"
@@ -322,7 +326,7 @@ virtual-media(){
       if [ -z "$iso_image" ]; then
         echo "Need to specify the ISO location."
       else
-        curl --globoff -L -w "%{http_code} %{url_effective}\\n" -ku "${username_password}" \
+        $CURL --globoff -L -w "%{http_code} %{url_effective}\\n" \
           -H "Content-Type: application/json" -H "Accept: application/json" \
           -d "{\"Image\": \"${iso_image}\"}" \
           -X POST "$bmc""$virtual_media_selected"/Actions/VirtualMedia.InsertMedia
@@ -330,7 +334,7 @@ virtual-media(){
     fi
 
     if [ "eject" = "$virtual_media_action" ]; then
-      curl --globoff -L -w "%{http_code} %{url_effective}\\n"  -ku "${username_password}" \
+      $CURL --globoff -L -w "%{http_code} %{url_effective}\\n" \
         -H "Content-Type: application/json" -H "Accept: application/json" \
         -d '{}'  -X POST "$bmc""$virtual_media_selected"/Actions/VirtualMedia.EjectMedia
     fi
@@ -339,7 +343,7 @@ virtual-media(){
 
 boot-once-from-cd() {
   local system=$(_system)
-  curl --globoff  -L -w "%{http_code} %{url_effective}\\n"  -ku ${username_password}  \
+  $CURL --globoff  -L -w "%{http_code} %{url_effective}\\n" \
   -H "Content-Type: application/json" -H "Accept: application/json" \
   -d '{"Boot":{ "BootSourceOverrideEnabled": "Once", "BootSourceOverrideTarget": "Cd" }}' \
   -X PATCH $system
@@ -347,8 +351,8 @@ boot-once-from-cd() {
 
 secure-boot(){
   local system=$(_system)
-  local secure_boot="$bmc"$(curl -sku "${username_password}" "$system" |jq -r '.SecureBoot."@odata.id"')
-  local enabled=$(curl -sku "${username_password}" "$secure_boot" |jq -r ".SecureBootEnable")
+  local secure_boot="$bmc"$($CURL -s "$system" |jq -r '.SecureBoot."@odata.id"')
+  local enabled=$($CURL -s "$secure_boot" |jq -r ".SecureBootEnable")
 
   if [ -z "$parameters" ]; then
     echo "$enabled"
@@ -358,7 +362,7 @@ secure-boot(){
     else
       if [ "true" = "$parameters" ] || [ "false" = "$parameters" ]; then
         local body="{\"SecureBootEnable\":$parameters}"
-        curl -sku "${username_password}" -X PATCH -H "Content-Type: application/json" -d "$body" "$secure_boot"
+        $CURL -s -X PATCH -H "Content-Type: application/json" -d "$body" "$secure_boot"
         echo "secure boot has been set as $parameters, you may need to reboot the node to take effect."
       else
         echo "$parameters is not supported, it should be true or false"
@@ -369,28 +373,28 @@ secure-boot(){
 
 storage(){
   local system=$(_system)
-  local storage="$bmc"$(curl -sku "${username_password}" "$system" |jq -r '.Storage."@odata.id"')
+  local storage="$bmc"$($CURL -s "$system" |jq -r '.Storage."@odata.id"')
   if [ -n "$parameters" ]; then
-    curl -sku "${username_password}" "$storage"/"$parameters" |jq
+    $CURL -s "$storage"/"$parameters" |jq
   else
-    curl -sku "${username_password}" "$storage" |jq
+    $CURL -s "$storage" |jq
   fi
 }
 
 root(){
   if [ -n "$parameters" ]; then
     attr=$(_pick $parameters)
-    curl -sku "${username_password}" "$bmc"/redfish/v1 |jq -r "pick(.$attr)"
+    $CURL -s "$bmc"/redfish/v1 |jq -r "pick(.$attr)"
   else
-    curl -sku "${username_password}" "$bmc"/redfish/v1 |jq
+    $CURL -s "$bmc"/redfish/v1 |jq
   fi
 }
 
 get(){
   if [ -n "$parameters" ]; then
-    curl -sku "${username_password}" "$bmc"$parameters |jq
+    $CURL -s "$bmc"$parameters |jq
   else
-    curl -sku "${username_password}" "$bmc"/redfish/v1 |jq
+    $CURL -s "$bmc"/redfish/v1 |jq
   fi
 }
 
@@ -421,7 +425,6 @@ else
   fi
 
   export bmc=$(yq ".bmc" "$CURRENT_SERVERS_CFG")
-  export username_password=$(yq ".userPass" "$CURRENT_SERVERS_CFG")
 
   $cmd
 fi
